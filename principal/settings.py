4cfd3738350890a5645718e3bd6ed1a296a32057
Cristiane <guilherme.mendes@dcomp.ufs.br>
2026-02-02 23:15:22 -0300

    configurando allowed_hosts e statics

--- principal/settings.py
+++ principal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*$ue6u@ub%w16wa%z*u3dskyoe3ysp!(d&_@8hc(_1=#7s(z81
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['coskika.pythonanywhere.com'] 
 
 
 # Application definition
@@ -126,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
